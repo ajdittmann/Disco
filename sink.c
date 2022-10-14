975c59478cac72f47efcae427248338389805115
Alexander Dittmann <dittmann@astro.umd.edu>
2022-10-13 22:19:19 -0400

    damping now works

--- sink.c
+++ sink.c
@@ -377,7 +377,7 @@ void cooling(const double *prim, double *cons,
       omtot = sqrt(omtot);
 
       ////direct implementation of source term
-      //cons[TAU] += rho*(enCurrent - enTarget)*dt*dV*beta*omtot;
+      //cons[TAU] -= rho*(enCurrent - enTarget)*dt*dV*omtot/beta;
 
       //integrate source term over timestep
       double Tm1 = expm1(-dt*omtot/beta);
@@ -465,7 +465,7 @@ void damping(const double *prim, double *cons, const double *xp,
         count = count + 1.0;
       }
     }
-    dampFactor = expm1(-dt*dampFactor);
+    dampFactor = expm1(-dt*ratetot);
     //dampFactor = -1.0*dt*dampFactor;
     double prims0[NUM_Q];
     double cons0[NUM_Q];
